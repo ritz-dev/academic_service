398804f045e227cad23e99b7f7b910df7fcb6bb9
ritz-dev <dev2@ritz.com.mm>
2025-02-14 10:09:26 +0630

    Change:Class

--- src/app/Http/Controllers/APIs/AcademicClassController.php
+++ src/app/Http/Controllers/APIs/AcademicClassController.php
@@ -22,43 +22,16 @@ class AcademicClassController extends Controller
 
     public function getAcademicClass(Request $request)
     {
+        $request->validate([
+            'academic_year_Id' => 'required',
+        ]);
+
         try {
-            $limit = $request->input('limit', 15);
-            $page = $request->input('page', 1);
-            $orderBy = $request->input('orderBy', 'academic_classes.created_at');
-            $sortedBy = $request->input('sortedBy', 'desc');
-            $search = $request->input('search', '');
-            $offset = ($page - 1) * $limit;
 
-            $validOrderColumns = ['academic_classes.created_at', 'academic_classes.updated_at'];
-            $validSortDirections = ['asc', 'desc'];
+            $dataArray = AcademicClass::where('academic_year_Id',);
+            $data = $dataArray->get();
 
-            $orderBy = in_array($orderBy, $validOrderColumns) ? $orderBy : 'academic_classes.created_at';
-            $sortedBy = in_array($sortedBy, $validSortDirections) ? $sortedBy : 'desc';
-
-            $dataArray = AcademicClass::with('sections')->selectRaw('ROW_NUMBER() OVER(ORDER BY '.$orderBy.' '.$sortedBy.') as number,
-                                academic_classes.id as id,
-                                academic_classes.name as name
-                            ')
-                            ->when($search, function ($query, $search) {
-                                $query->where(function ($query) use ($search) {
-                                    $query->where('academic_classes.name', 'like', "%{$search}%");
-                                });
-                            });
-
-            $total = $dataArray->get()->count();
-
-            $data = $dataArray
-                ->orderBy($orderBy, $sortedBy)
-                ->skip($offset)
-                ->take($limit)
-                ->get();
-
-            return response()->json([
-                "message" => "success",
-                "total" => $total,
-                "data" => $data
-            ]);
+            return response()->json([$data]);
         } catch (Exception $e) {
             return response()->json([
                 'success' => false,
@@ -84,40 +57,23 @@ class AcademicClassController extends Controller
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'academicYear' => 'required|string|max:255',
-            'section' => 'required|array|min:1',
-            'section.*.name' => 'required|string|max:255',
-            'subjects' => 'required|array|min:1',
-            'subjects.*.name' => 'required|string|max:255',
-            'subjects.*.id' => 'required|integer|exists:subjects,id',
         ], [
             'name.unique' => 'This class already exists for the selected academic year'
         ]);
 
-        DB::beginTransaction();
-
         try{
             $class = AcademicClass::create([
                 'id' => Str::uuid(),
-                'name' => $request->input('name')
+                'name' => $request->input('name'),
+                'academic_year_id' => $request->input('academicYear')
             ]);
 
-            foreach($validated['section'] as $section) {
-                Section::create([
-                    'id' => Str::uuid(),
-                    'name' => $section['name'],
-                    'academic_year_id' => $validated['academicYear'],
-                    'academic_class_id' => $class->id,
-                    'teacher_id' => 1
-                ]);
-            }
+            $data = AcademicClass::where('academic_year_id', $request->input('academicYear'))
+                ->select(['id', 'name'])
+                ->get();
 
-            DB::commit();
-
-            return response()->json([
-                'id' => $validated['academicYear']
-            ]);
+            return response()->json([$data]);
         } catch (Exception $e) {
-            DB::rollBack();
             return $this->handleException($e, 'Failed to create section');
         }
     }
