eb952b971106808968ba7a6b2397823f5e28b4de
ritz-dev <dev2@ritz.com.mm>
2025-02-17 10:30:25 +0630

    Add: Get Class

--- src/app/Http/Controllers/APIs/AcademicClassController.php
+++ src/app/Http/Controllers/APIs/AcademicClassController.php
@@ -109,7 +109,7 @@ class AcademicClassController extends Controller
     public function showClass(Request $request){
         try{
             $class_id = $request->slug;
-            $academic_class = AcademicClass::with('academicYear')->where('id',$class_id)->get();
+            $academic_class = AcademicClass::with('academicYear')->where('id',$class_id)->first();
             return response()->json($academic_class, 200);
         }catch (Exception $e){
             return $this->handleException($e, 'Failed to fetch the class');
