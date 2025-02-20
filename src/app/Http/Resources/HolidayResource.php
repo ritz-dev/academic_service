c112df94bf48067b91e6f73dd59e4dd051f412cb
ritz-dev <dev2@ritz.com.mm>
2025-02-20 09:41:48 +0630

    Change: TimeTable Route

--- src/app/Http/Resources/HolidayResource.php
+++ src/app/Http/Resources/HolidayResource.php
@@ -15,6 +15,7 @@ class HolidayResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
         ];
     }
